b85e815a3ea1e35911df725ee94c7e8e411fc913
Berna UZUNOĞLU <bernauzunoglu58@gmail.com>
2024-08-06 18:54:43 +0300

    Requirements reveal slides removed.

--- Streamlit_Design.py
+++ Streamlit_Design.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import reveal_slides as rs
 import streamlit.components.v1 as components
 from streamlit_carousel import carousel
 import get_youtube_playlist_songs as youtube
